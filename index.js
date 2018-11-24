e998ad6cce03bf698b2b9ab9692ffcaf83125f2a
chensheng <chensheng@kezaihui.com>
2018-11-24 14:21:43 +0800

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+require('babel-register');
+const babel = require('@babel/core');
+const babelPresetLatestNode = require('babel-preset-latest-node');
+
+babel.transform('code();', {
+  presets: [[babelPresetLatestNode, {
+    target: 'current',
+  }]],
+});
+
+require('babel-polyfill');
+require('./src');
