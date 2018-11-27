a23258c6e0279da9efc68a189b6a4d28ee626a7c
cslove <969118892@qq.com>
2018-11-27 20:31:32 +0800

    index

--- index.js
+++ index.js
@@ -11,13 +11,4 @@ babel.transform('code();', {
 });
 
 require('babel-polyfill');
-const ora = require('ora');
-
-const spinner = ora('Loading unicorns');
-spinner.color = 'yellow';
-spinner.text = '加载中ing。。。';
-spinner.start();
-setTimeout(() => {
-  spinner.succeed('成功～');
-}, 1000);
 require('./src');
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import a from './a';
+import ora from 'ora';
+import program from 'commander';
+import 'color';
+import { version } from '../package.json';
 
-a.a();
+const spinner = ora();
+let fileName = '';
+let filePath = '';
+
+program
+  .version(version, '-v --version')
+  .arguments('<name> [path]')
+  .action((name, path) => {
+    fileName = name;
+    filePath = path;
+  });
+program.parse(process.argv);
+if (fileName === '') {
+  spinner.color = 'yellow';
+  spinner.fail('djaklfj');
+}
