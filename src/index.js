57c15f10f021e103c2b005e8d7feaf95738cce57
cslove <969118892@qq.com>
2018-11-28 20:25:16 +0800

    utils

--- src/index.js
+++ src/index.js
@@ -2,20 +2,29 @@ import ora from 'ora';
 import program from 'commander';
 import 'color';
 import { version } from '../package.json';
+import { execCommandFile } from './utils';
 
-const spinner = ora();
+const spinner = ora('正在创建ing...').start();
 let fileName = '';
-let filePath = '';
+// let filePath = '';
+const reg = [/.vue$/];
 
 program
   .version(version, '-v --version')
   .arguments('<name> [path]')
   .action((name, path) => {
     fileName = name;
-    filePath = path;
+    // filePath = path;
   });
 program.parse(process.argv);
 if (fileName === '') {
-  spinner.color = 'yellow';
-  spinner.fail('djaklfj');
+  spinner.fail('请至少提供一个文件名称哟～');
+} else {
+  reg.forEach((i) => {
+    if (i.test(fileName)) {
+      execCommandFile(fileName);
+    }
+  });
 }
+// if (existsSync(resolve(__dirname, `command/${filename}.js`)))
+// spinner.succeed('创建成功');
